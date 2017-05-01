3b8765c8dc9e93526d430235e9c1d0241a4cd0f1
vikneshwaran1994 <vikneshwaran1994@gmail.com>
2017-04-30 20:21:15 -0400

    add the files

---
+++ projectbikewindsor/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title >Community Cycling Survey</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-frontpage.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!--<a class="navbar-brand" href="#">Start Bootstrap</a>-->
+            </div>
+            
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Image Background Page Header -->
+    <!-- Note: The background image is set within the business-casual.css file. -->
+    <header class="business-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                                 </div>
+            </div>
+        </div>
+    </header>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+    
+        <hr>
+<!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">About</a>
+                    </li>
+                                        <li>
+                        <a href="accident.html">Accidents</a>
+                    </li>
+                    <li>
+                        <a href="cyhabits.html">Accident Causes</a>
+                    </li>
+                    <li>
+                        <a href="trafficact.html">Traffic Act</a>
+                    </li>
+                    <li>
+                        <a href="rentalsetup.html">Rental Setup</a>
+                    </li>
+                    <li>
+                        <a href="physicalactivity.html">Physical Activity</a>
+                    </li>
+                    <li>
+                        <a href="coursesinterested.html">Courses Interested</a>
+                    </li>
+                    <li>
+                        <a href="bikestolen.html">Bikes Stolen</a>
+                    </li>
+                    <li>
+                        <a href="Ridemoreif.html">Ride More If</a>
+                    </li>
+
+                </ul>
+            </div>
+        <div class="row">
+            <div class="col-sm-8">
+                <h2>About</h2 >
+                <p>Bike Windsor Essex is a member-based advocacy group working to help make our region safer and more bike friendly. Bike Windsor Essex recently published a community survey with 25 questions about cycling and received over 500 responses.
+This website takes this data and analyzes it in order to make the complex simple and the abstract tangible. 
+                    <br> <br>Click on the tabs above to learn more! </p>
+                
+            </div>
+            
+        </div>
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
